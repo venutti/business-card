fcead9e0932d590f60664bf3ebc505353d536eef
venutti <lvenutti@fi.uba.ar>
2023-01-23 14:46:53 -0300

    Agrega componente About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about--container">
+      <article className="about--article">
+        <h2 className="about--title">About</h2>
+        <p className="about--text">
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </article>
+      <article className="about--article">
+        <h2 className="about--title">Interests</h2>
+        <p className="about--text">
+          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+      </article>
+    </div>
+  );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Info from "./Info";
 import About from "./About";
-import Interests from "./Interests";
+import Footer from "./Footer";
 
 export default function Card() {
   return (
     <div className="card">
       <Info />
       <About />
-      <Interests />
+      <Footer />
     </div>
   );
 }
